2e1a29b1f7f37b91e420947f4c4a2963d6d6fac3
vacharbhargav008 <vismay.bhargav@gmail.com>
2024-06-03 19:46:06 -0700

    Init template mod

---
+++ src/main/java/net/vismayb/toxtweaks/ToxTweaksDataGenerator.java
@@ -0,0 +1,11 @@
+package net.vismayb.toxtweaks;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class ToxTweaksDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
